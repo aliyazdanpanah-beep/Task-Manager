8e7f330ce0c8ba524b597f5f6809c8fc4d1fbfe6
aliyazdanpanah-beep <aliyazdanpanahfard@gmail.com>
2026-06-26 15:33:12 +0330

    adding task API

--- backend/main.py
+++ backend/main.py
@@ -1,6 +1,6 @@
 from fastapi import FastAPI, Path, Query, status
 from dependecis import db_dependency
-from models import Users
+from models import Users, Task
 
 app = FastAPI()
 
@@ -12,3 +12,8 @@ async def get_all_tasks():
 @app.get('/users')
 async def get_all_users(db: db_dependency):
    return db.query(Users).all()
+
+
+@app.get('/tasks')
+async def get_all_tasks(db: db_dependency):
+   return db.query(Task).all()
