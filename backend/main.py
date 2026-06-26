04f5e8e2f55f2165910252eaecad65b1c61cd8ee
aliyazdanpanah-beep <aliyazdanpanahfard@gmail.com>
2026-06-26 15:30:50 +0330

    update

--- backend/main.py
+++ backend/main.py
@@ -1,7 +1,14 @@
 from fastapi import FastAPI, Path, Query, status
+from dependecis import db_dependency
+from models import Users
 
 app = FastAPI()
 
 @app.get('/')
 async def get_all_tasks():
    return {'massage':'wellcom Task Manager'}
+
+
+@app.get('/users')
+async def get_all_users(db: db_dependency):
+   return db.query(Users).all()
